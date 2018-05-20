04fc114d72b92780900e281d09bb93795ee3ee37
aguscha <artur.guschakowski@googlemail.com>
2018-05-20 09:45:38 +0200

    update urls of docker files

--- installElasticsearch.sh
+++ installElasticsearch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # assumes you're already logged in and in the correct project
-oc new-app https://github.com/dougchansolute/openshift3-elk --context-dir=elasticsearch --name=elasticsearch
+oc new-app https://github.com/aguscha/openshift3-elk6 --context-dir=elasticsearch --name=elasticsearch
